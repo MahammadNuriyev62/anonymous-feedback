7263f9c2c02d19a054f87c96ebcd0d6c6b9b74c7
MahammadNuriyev62 <maganuriyev@gmail.com>
2025-08-06 15:19:35 +0400

    preview image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          property="og:image"
+          content={
+            "https://i.pinimg.com/564x/33/de/97/33de974459778d84e4831821cd34914c.jpg"
+          }
+        />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
